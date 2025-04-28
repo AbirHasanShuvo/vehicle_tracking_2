051c48c4c508713c5f8933c3eb336c66a7a0e487
Md. Abir Hasan <abirhasanshuvo789@gmail.com>
2025-04-28 14:23:48 +0600

    permission handling completed

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -9,6 +10,7 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  Location _locationController = new Location();
   static const LatLng _pGooglePlex = LatLng(
     23.778036640980943,
     90.40585156395386,
@@ -17,6 +19,14 @@ class _MapPageState extends State<MapPage> {
     23.822669356565928,
     90.45480580828423,
   );
+  LatLng? _currentP = null;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationUpdates();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,5 +46,39 @@ class _MapPageState extends State<MapPage> {
         },
       ),
     );
+  }
+
+  Future<void> getLocationUpdates() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if (_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+    } else {
+      return;
+    }
+
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    _locationController.onLocationChanged.listen((
+      LocationData currentlocation,
+    ) {
+      if (currentlocation.latitude != null &&
+          currentlocation.longitude != null) {
+        setState(() {
+          _currentP = LatLng(
+            currentlocation.latitude!,
+            currentlocation.longitude!,
+          );
+          print(_currentP);
+        });
+      }
+    });
   }
 }
