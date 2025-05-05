f2cf1cb39ad8d9f8a859c6b10ca0ce119ec39dc5
Md. Abir Hasan <abirhasanshuvo789@gmail.com>
2025-05-05 17:01:37 +0600

    user location identified successfully

--- lib/mapbox/pages/home_page.dart
+++ lib/mapbox/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,8 +11,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  MapboxMap? mapboxMapController;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: MapWidget(onMapCreated: (controller) {}));
+    return Scaffold(body: MapWidget(onMapCreated: _onMapCreated));
+  }
+
+  // void _onMapCreated(MapboxMap controller) {
+  //   setState(() {
+  //     mapboxMapController = controller;
+  //   });
+
+  //   mapboxMapController!.location.updateSettings(
+  //     LocationComponentSettings(enabled: true, pulsingEnabled: true),
+  //   );
+  // }
+
+  void _onMapCreated(MapboxMap controller) async {
+    setState(() {
+      mapboxMapController = controller;
+    });
+
+    var status = await Permission.location.request();
+    if (status.isGranted) {
+      mapboxMapController!.location.updateSettings(
+        LocationComponentSettings(enabled: true, pulsingEnabled: true),
+      );
+    } else {
+      // You can handle denied permission here
+      debugPrint("Location permission not granted");
+    }
+  }
+
+  Future<void> _setupPositionTracking() async {
+    bool serviceEnabled;
+    LocationPermission permission;
   }
 }
